a03d07c86e32b9bfc6a25f06d961227017f3c9b0
Azamat Tolegenov <azicus.sdu@gmail.com>
2024-01-09 01:05:42 +0600

    Added third line

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("My first line of code I changed");
         System.out.println("My second line");
+        System.out.println("My third line");
     }
 }
