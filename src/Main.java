bc89fcd0d962910bedb40e70780979a60c7843ba
Azamat Tolegenov <azicus.sdu@gmail.com>
2024-01-09 01:10:42 +0600

    Added forth line

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main {
         System.out.println("My first line of code I changed");
         System.out.println("My second line");
         System.out.println("My third line");
+        System.out.println("My forth line");
     }
 }
